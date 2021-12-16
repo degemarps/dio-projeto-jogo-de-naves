c5a46806536eeb7cfab16a0ac8535732ec09e8e9
degemarps <degemarpereira@gmail.com>
2021-12-16 13:01:33 -0300

    Adicionando a funçao de disparo

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function start() { // Inicio da função start()
   var jogo = {};
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
+  var podeAtirar = true;
   var TECLA = {
     W: 87,
     S: 83,
@@ -42,7 +43,6 @@ function start() { // Inicio da função start()
     moveinimigo1();
     moveinimigo2();
     moveamigo();
-
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -78,7 +78,8 @@ function start() { // Inicio da função start()
 
     if (jogo.pressionou[TECLA.D]) {
 
-      //Chama função Disparo	
+      //Chama função Disparo
+      disparo();
     }
 
   } // fim da função movejogador()
@@ -120,4 +121,37 @@ function start() { // Inicio da função start()
     }
 
   } // fim da função moveamigo()
+
+  function disparo() {
+
+    if (podeAtirar == true) {
+
+      podeAtirar = false;
+
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX = parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro = topo + 51;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top", topoTiro);
+      $("#disparo").css("left", tiroX);
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30);
+
+    } //Fecha podeAtirar
+
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left", posicaoX + 15);
+
+      if (posicaoX > 900) {
+
+        window.clearInterval(tempoDisparo);
+        tempoDisparo = null;
+        $("#disparo").remove();
+        podeAtirar = true;
+
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
 } // Fim da função start
